9708567dfc7bd8eb7934587d4ddc1927ba694de6
telontelvarost <telontelvarost@gmail.com>
2024-02-28 22:46:23 -0500

    UDOC-0 - Update GCAPI and read configs before mixins to configure chat and sign settings

--- src/main/java/pl/telvarost/mojangfixstationapi/mixin/MojangFixStationApiMixinPlugin.java
+++ src/main/java/pl/telvarost/mojangfixstationapi/mixin/MojangFixStationApiMixinPlugin.java
@@ -15,11 +15,19 @@
 
 package pl.telvarost.mojangfixstationapi.mixin;
 
+import blue.endless.jankson.Jankson;
+import blue.endless.jankson.JsonObject;
+import blue.endless.jankson.api.SyntaxError;
 import net.fabricmc.loader.api.FabricLoader;
+import net.glasslauncher.mods.api.gcapi.api.GCAPI;
+import net.modificationstation.stationapi.api.util.Identifier;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import pl.telvarost.mojangfixstationapi.Config;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
@@ -27,10 +35,37 @@ public final class MojangFixStationApiMixinPlugin implements IMixinConfigPlugin 
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        try {
+            JsonObject configObject = Jankson
+                    .builder()
+                    .build()
+                    .load(new File("config/mojangfixstationapi", "config.json"));
+
+            Config.ConfigFields.enableChatChanges = configObject.getBoolean("enableChatChanges", true);
+            Config.ConfigFields.enableWoodenSignChanges = configObject.getBoolean("enableWoodenSignChanges", true);
+        } catch (IOException ex) {
+            System.out.println("Couldn't read the config file" + ex.toString());
+        } catch (SyntaxError error) {
+            System.out.println("Couldn't read the config file" + error.getMessage());
+            System.out.println(error.getLineMessage());
+        }
+
         if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.inventory.ContainerScreenMixin")) {
             return !FabricLoader.getInstance().isModLoaded("inventorytweaks");
         } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.inventory.PlayerEntityMixin")) {
             return !FabricLoader.getInstance().isModLoaded("inventorytweaks");
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.chat.ChatScreenMixin")) {
+            return Config.ConfigFields.enableChatChanges;
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.text.chat.SleepingChatScreenMixin")) {
+            return Config.ConfigFields.enableChatChanges;
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.text.sign.ClientNetworkHandlerMixin")) {
+            return Config.ConfigFields.enableWoodenSignChanges;
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.text.sign.SignBlockEntityMixin")) {
+            return Config.ConfigFields.enableWoodenSignChanges;
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.text.sign.SignBlockEntityRendererMixin")) {
+            return Config.ConfigFields.enableWoodenSignChanges;
+        } else if (mixinClassName.equals("pl.telvarost.mojangfixstationapi.mixin.client.text.sign.SignEditScreenMixin")) {
+            return Config.ConfigFields.enableWoodenSignChanges;
         } else {
             return true;
         }
