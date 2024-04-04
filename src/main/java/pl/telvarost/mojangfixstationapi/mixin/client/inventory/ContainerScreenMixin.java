7500a7b067d2c9d7b19c1ddfa360ee942f43551d
telontelvarost <telontelvarost@gmail.com>
2024-04-03 20:08:06 -0400

    UDOC-0 - Fix container offset when swapping items to hotbar slots

--- src/main/java/pl/telvarost/mojangfixstationapi/mixin/client/inventory/ContainerScreenMixin.java
+++ src/main/java/pl/telvarost/mojangfixstationapi/mixin/client/inventory/ContainerScreenMixin.java
@@ -104,10 +104,14 @@ public abstract class ContainerScreenMixin extends Screen {
         }
 
         if (keyCode >= Keyboard.KEY_1 && keyCode <= Keyboard.KEY_9) {
-            if (this.minecraft.player.inventory.getCursorStack() == null)
+            if (  (null != this.container.slots)
+               && (10 <= this.container.slots.size())
+            ) {
+                if (this.minecraft.player.inventory.getCursorStack() == null)
+                    this.minecraft.interactionManager.clickSlot(this.container.syncId, slot.id, 0, false, this.minecraft.player);
+                this.minecraft.interactionManager.clickSlot(this.container.syncId, (this.container.slots.size() - 10) + keyCode - 1, 0, false, this.minecraft.player);
                 this.minecraft.interactionManager.clickSlot(this.container.syncId, slot.id, 0, false, this.minecraft.player);
-            this.minecraft.interactionManager.clickSlot(this.container.syncId, 35 + keyCode - 1, 0, false, this.minecraft.player);
-            this.minecraft.interactionManager.clickSlot(this.container.syncId, slot.id, 0, false, this.minecraft.player);
+            }
         }
     }
 }
